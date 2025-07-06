1194fc88dc00f4bf235d9d54532828633ac8ee0c
Seungyu Ryu <qbysy921219@gmail.com>
2025-07-06 12:26:06 +0900

    Update hello.py

--- week1-basic/hello.py
+++ week1-basic/hello.py
@@ -1,1 +1,12 @@
+# 내 첫 번째 python 코드
+print("안녕하세요! 저는 류승유입니다.")
+print("저는 화학공학과 학생이고 python을 배우고 있습니다.")
 
+# 변수 연습
+name = '승유'
+university = '광운대학교'
+major = '화학공학과'
+
+print(f"이름:{name}")
+print(f"대학:{university}")
+print(f"전공:{major}")
